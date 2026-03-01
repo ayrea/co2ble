32fde8c1666a37430927c76730b48f7be70bc0fc
aayre <>
2026-03-01 09:32:10 +0800

    Cleaner code for ESP32

--- micro/src/main.cpp
+++ micro/src/main.cpp
@@ -1,14 +1,21 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-#define SDA = 21 // Data
-#define SCL = 22 // Clock
+#define SDA 21 // Data
+#define SCL 22 // Clock
+
+#define SENSOR_I2C_ADDR 0x62
+#define CMD_START_PERIODIC_MSB 0x21
+#define CMD_START_PERIODIC_LSB 0xb1
+#define MEASUREMENT_BYTES 9
+
+#define SCD4X_RAW_SCALE 65536.0f
+#define SCD4X_TEMP_OFFSET -45.0f
+#define SCD4X_TEMP_SCALE 175.0f
+#define SCD4X_HUM_SCALE 100.0f
 
 #define CRC8_POLYNOMIAL 0x31
 #define CRC8_INIT 0xFF
-
-struct tm localTimeInfo;
-uint32_t localTimeMs;
 
 uint16_t temperature;
 uint16_t humidity;
@@ -55,60 +62,78 @@ void setup()
 {
   Serial.begin(115200);
   delay(2000);
-  Wire.begin();
+  Wire.begin(SDA, SCL);
 
   delay(1000);
 
-  // Start start_periodic_measurement
-  Wire.beginTransmission(0x62);
-  Wire.write(byte(0x21));
-  Wire.write(byte(0xb1));
-  Wire.endTransmission();
-
-  Serial.println("Setup wire");
+  // Start periodic measurement (SCD4x 0x21 0xb1)
+  Wire.beginTransmission(SENSOR_I2C_ADDR);
+  Wire.write(byte(CMD_START_PERIODIC_MSB));
+  Wire.write(byte(CMD_START_PERIODIC_LSB));
+  uint8_t err = Wire.endTransmission();
+  if (err != 0)
+  {
+    Serial.print("Sensor not found (I2C error ");
+    Serial.print(err);
+    Serial.println(")");
+  }
+  else
+  {
+    Serial.println("Setup wire");
+  }
   delay(5000);
 }
 
 void loop()
 {
-  byte buffer[9];
-  Wire.requestFrom(0x62, sizeof(buffer));
+  byte buffer[MEASUREMENT_BYTES];
+  size_t received = Wire.requestFrom((uint8_t)SENSOR_I2C_ADDR, (size_t)sizeof(buffer));
 
-  for (int i = 0; i < sizeof(buffer); i++)
-  {
-    buffer[i] = Wire.read();
-  }
-
-  // Check CRCs
-  if (ValidCRC(buffer))
-  {
-    // Get CO2 value
-    co2 = buffer[0] * 255 + buffer[1];
-
-    // Get temperature value
-    float temp = ((float)buffer[3] * (float)255 + (float)buffer[4]) / (float)65536;
-    float temp1 = (float)-45 + ((float)175 * temp);
-    temperature = temp1 * (float)100;
-
-    // Get humidity value
-    float hum = (((float)buffer[6] * (float)255 + (float)buffer[7]) / (float)65536) * 100;
-    humidity = hum * (float)100;
-  }
-  else
+  if (received != sizeof(buffer))
   {
     co2 = 0;
     temperature = 0;
     humidity = 0;
+    Serial.println("I2C read failed (wrong byte count)");
+  }
+  else
+  {
+    for (size_t i = 0; i < sizeof(buffer); i++)
+    {
+      buffer[i] = Wire.read();
+    }
+
+    // Check CRCs
+    if (ValidCRC(buffer))
+    {
+      // Get CO2 value
+      co2 = buffer[0] * 256 + buffer[1];
+
+      // Get temperature value
+      float temp = ((float)buffer[3] * 256.0f + (float)buffer[4]) / SCD4X_RAW_SCALE;
+      float temp1 = SCD4X_TEMP_OFFSET + (SCD4X_TEMP_SCALE * temp);
+      temperature = (uint16_t)(temp1 * 100.0f);
+
+      // Get humidity value
+      float hum = (((float)buffer[6] * 256.0f + (float)buffer[7]) / SCD4X_RAW_SCALE) * SCD4X_HUM_SCALE;
+      humidity = (uint16_t)(hum * 100.0f);
+    }
+    else
+    {
+      co2 = 0;
+      temperature = 0;
+      humidity = 0;
+    }
   }
 
   Serial.print("CO2: ");
   Serial.println(co2);
 
-  float ftemp = (float)temperature / (float)100;
+  float ftemp = (float)temperature / 100.0f;
   Serial.print("Temperature: ");
   Serial.println(String(ftemp, 1));
 
-  float fhum = (float)humidity / (float)100;
+  float fhum = (float)humidity / 100.0f;
   Serial.print("Humidity: ");
   Serial.println(String(fhum, 0));
 
